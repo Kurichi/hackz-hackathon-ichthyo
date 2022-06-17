863d23684f682f8301fca23b4011ea80bfb3c520
Kurichi <yuuyapp080@gmail.com>
2022-06-17 23:25:12 +0900

    initial commit

---
+++ backend/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+#include "server.hpp"
+
+int main(int argc, char** argv) {
+  // コマンドライン引数を扱いやすい vector<string> に入れる
+  std::vector<std::string> args(argv, argv + argc);
+
+  // コマンドライン引数によって処理を変更する
+  // -debug : debug mode
+
+  Server server;
+
+  // Server Initialize
+
+  // Server Run
+
+  return 0;
+}
