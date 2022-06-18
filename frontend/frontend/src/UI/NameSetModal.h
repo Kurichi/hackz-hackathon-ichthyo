1d449d1a7f97fe4208857ff0cc7540ca81035e84
TOMOQ1024 <tomo_suzu_16384@au.com>
2022-06-19 03:13:48 +0900

    [add] NameSetModal.hの追加

---
+++ frontend/frontend/src/UI/NameSetModal.h
@@ -0,0 +1,28 @@
+﻿#pragma once
+#include <Siv3D.hpp>
+
+// 名前設定用モーダルクラス
+
+class NameSetModal {
+private:
+	//
+public:
+
+	// コンストラクタ
+	const NameSetModal()
+	{
+		//
+	}
+
+	// 更新
+	void update()
+	{
+		//
+	}
+
+	// 描画
+	void draw() const
+	{
+		//
+	}
+};
