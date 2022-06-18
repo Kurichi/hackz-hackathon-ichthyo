f8b7eb4b88b08f13a18d0c0c31737cf16f86ab8c
Kurichi <yuuyapp080@gmail.com>
2022-06-18 11:32:16 +0900

    add function format

--- backend/server.cpp
+++ backend/server.cpp
@@ -1,2 +1,10 @@
 #include "server.hpp"
 
+// 初期化処理
+Server::Server() {}
+
+void Server::start() {}
+
+void Server::receive() {}
+
+void Server::broadcast() {}
--- backend/server.hpp
+++ backend/server.hpp
@@ -1,10 +1,23 @@
 #ifndef _SERVER_H_
 #define _SERVER_H_
 
+#include <boost/array.hpp>
+#include <boost/asio.hpp>
+#include <iostream>
+#include <string>
+
 class Server {
  private:
+  std::vector<boost::asio::ip::udp::endpoint> endpointList;
+
  public:
   Server();
+
+  void start();
+
+  void receive();
+
+  void broadcast();
 };
 
 #endif  // _SERVER_H_
