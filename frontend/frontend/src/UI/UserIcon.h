23378d056e8b4a5bbb54c6cdf74bd6717ee49581
yakishamo <82935423+yakishamo@users.noreply.github.com>
2022-06-19 13:17:51 +0900

    [add] アイコンの明るさを通話が通じてるときの明るくするように

--- frontend/frontend/src/UI/UserIcon.h
+++ frontend/frontend/src/UI/UserIcon.h
@@ -1,6 +1,8 @@
 ﻿#pragma once
 #include <Siv3D.hpp>
 
+extern JudgeVoice jv;
+
 // ユーザーのアイコン等
 
 class UserIcon {
@@ -42,7 +44,9 @@ public:
 		circle(img(85, 0, 120, 120)).draw();
 		font(name).draw(Arg::center = center.movedBy(0, radius + 30), Palette::Black);
 		circle.stretched(5).drawArc(0, Math::Pi * 2 * volume, 0, 10, ColorF(1.0, 1.0 - volume, 1.0 - volume));
-		if (volume < 1.0) {
+		bool a = (volume < 1.0);
+		if ((jv.GetType() & L_AVE_AMP & L_PITCH) == 0) a = !a;
+		if (a) {
 			circle.draw(ColorF(0.0, 0.7));
 		}
 	}
