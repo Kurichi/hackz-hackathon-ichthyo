72d1d1c1c1c90778944b7c4cd832bf4cdaca6111
rockcutter <rockcutter414@gmail.com>
2022-06-19 09:13:39 +0900

    [add] add mute

--- frontend/frontend/src/Core/Audio/UserVoiceRecorder.h
+++ frontend/frontend/src/Core/Audio/UserVoiceRecorder.h
@@ -14,13 +14,22 @@ private:
 	double previousSendedTime;
 	boost::asio::ip::udp::endpoint serverEndpoint;
 	std::shared_ptr<boost::asio::ip::udp::socket> socket;
-	boost::asio::io_service ios;
-	boost::asio::ip::udp::socket sock;
 	std::queue<Wave> VoiceQueue;
 	std::unique_ptr<std::thread> SendVoice;
 	const User& myUser;
+	bool muteFlag;
+	//いらない
+	boost::asio::io_service ios;
+	boost::asio::ip::udp::socket sock;
 public:
 	UserVoiceRecorder(const boost::asio::ip::udp::endpoint& serverEndpoint, const User& myUser);
 	void SendAudioData();
+	bool ToggleMute();
 };
+
+inline bool UserVoiceRecorder::ToggleMute() {
+	this->muteFlag = !(this->muteFlag);
+	return this->muteFlag;
+}
+
 
