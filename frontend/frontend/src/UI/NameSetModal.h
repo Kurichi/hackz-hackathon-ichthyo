9788a7e4e0e8b2fb1b6d28eb6c3be723bfbc38fb
TOMOQ1024 <tomo_suzu_16384@au.com>
2022-06-19 09:59:19 +0900

    [add] 音量の取得, NameSetModal.hの実装(途中)

--- frontend/frontend/src/UI/NameSetModal.h
+++ frontend/frontend/src/UI/NameSetModal.h
@@ -5,24 +5,45 @@
 
 class NameSetModal {
 private:
-	//
+
+	Font font = Font(30);
+	String text;
+	Rect rect;
+	s3d::RoundRect rrect;
+
 public:
+
+	bool show = 0;
 
 	// コンストラクタ
 	const NameSetModal()
 	{
-		//
+		rect = Rect(
+			Scene::Center().x, 300,
+			int(Scene::Width() * 0.7), 400
+		);
+		rrect = s3d::RoundRect(rect, 10);
 	}
 
 	// 更新
-	void update()
+	void update(const bool u)
 	{
-		//
+		TextInput::UpdateText(text);
 	}
 
 	// 描画
 	void draw() const
 	{
-		//
+		rrect.draw(ColorF(1.0));
+		font(text).draw(rect.stretched(-20));
 	}
 };
+
+/*
+
+To Do
+・名前変更モーダルの実装
+・発言者のソート(線形補完でアニメーション)
+(・アイコン変更モーダルの実装(テンプレ))
+
+*/
